1752e86e7808ae836eaa7a6a80ad634f171433f7
siddharth-prodapt <siddharth.sp@prodapt.com>
2023-08-24 11:13:41 +0530

    main driver class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+import restaurant.RestaurantManager;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int option = -1;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("HOME");
+			System.out.println("1. Restaurant\n2. User\n3. Admin");
+			System.out.println("Select user type: ");
+			option = sc.nextInt();
+			
+			switch(option) {
+			case 1:
+				RestaurantManager.restaurantAuthMenu();
+				break;
+			case 2: break;
+			case 3: break;
+			default: 
+				System.out.println("Select correct option!");
+			}
+		}while(option!=4);
 
 	}
 
--- src/restaurant/model/Restaurant.java
+++ src/restaurant/model/Restaurant.java
@@ -10,11 +10,11 @@ public class Restaurant {
 	private String emailId;
 	private String userId;
 	private String password;
-	
-	public Restaurant( String restaurantName, String address, String location, String city,
-			String state, String userId, String password, String emailId) {
+
+	public Restaurant(String restaurantName, String address, String location, String city, String state, String userId,
+			String password, String emailId) {
 		super();
-		
+
 		this.restaurantName = restaurantName;
 		this.Address = address;
 		this.location = location;
@@ -25,66 +25,76 @@ public class Restaurant {
 		this.emailId = emailId;
 	}
 
-	
 	public int getRestaurantId() {
 		return restaurantId;
 	}
+
 	public void setRestaurantId(int restaurantId) {
 		this.restaurantId = restaurantId;
 	}
+
 	public String getRestaurantName() {
 		return restaurantName;
 	}
+
 	public void setRestaurantName(String restaurantName) {
 		this.restaurantName = restaurantName;
 	}
-	
+
 	public String getAddress() {
 		return Address;
 	}
+
 	public void setAddress(String address) {
 		Address = address;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getState() {
 		return state;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 
 	public String getEmailId() {
 		return emailId;
 	}
 
-
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
-	
-	
+
 }
