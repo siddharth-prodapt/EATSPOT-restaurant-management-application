891fc9cde7ad2eede080fc38f37c690385a46cf5
siddharth-prodapt <siddharth.sp@prodapt.com>
2023-08-27 15:37:32 +0530

    DB service and Driver class

---
+++ src/table/TableDBServices.java
@@ -0,0 +1,27 @@
+package table;
+
+public class TableDBServices {
+	void createTable() {
+		
+		
+	}
+	
+	void getTableDetails() {
+		
+	}
+	
+	void getTableDetailsById(int id) {
+		
+	}
+	
+	
+	void updateTableDetails() {
+		
+	}
+	
+	
+	
+	void deleteTableDetails() {
+		
+	}
+}
